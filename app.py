42219a5323b93787e0e9ee83a1781b94d58088e1
Dixon-Antony <dixon.j.antony2001@gmail.com>
2023-02-25 16:35:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/listings")
+def listings():
+    return render_template('listings.html')
+
+@app.route("/viewListing")
+def viewListing():
+    return render_template('viewListing.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/favorites")
+def favorites():
+    return render_template('favorites.html')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+
+if __name__ == "__main__":
+  app.run()
